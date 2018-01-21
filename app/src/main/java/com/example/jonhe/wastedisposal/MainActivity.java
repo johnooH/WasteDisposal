58ed340b8805dcd3cc8dad9b9e3ffe800eeca094
sclark13 <sam.h.clark@gmail.com>
2018-01-20 17:54:15 -0800

    Trying to commit from Android Studio

--- app/src/main/java/com/example/jonhe/wastedisposal/MainActivity.java
+++ app/src/main/java/com/example/jonhe/wastedisposal/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.jonhe.wastedisposal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//test commit
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
